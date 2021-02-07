d244995578a721fafee688e77740220b8459ff7b
Clem <valles.clement@gmail.com>
2021-02-07 14:25:09 -0600

    Adding the html route page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,10 +6,12 @@ router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+//Exercice page
 router.get("/exercise", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+//Statistics page
 router.get("/stats", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
